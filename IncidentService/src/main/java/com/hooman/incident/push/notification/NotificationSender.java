75d05655d4319ded156fc6d0ee6012c2c75777f8
piyush.joshi <piyush.joshi@timesinternet.in>
2019-05-03 12:14:18 +0530

    fix

--- IncidentService/src/main/java/com/hooman/incident/push/notification/NotificationSender.java
+++ IncidentService/src/main/java/com/hooman/incident/push/notification/NotificationSender.java
@@ -6,11 +6,15 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.jooq.tools.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.hooman.incident.controller.IncidentController;
 
 /**
  * @author shubham.agarwal
@@ -28,6 +32,8 @@ public class NotificationSender {
 	private static final String FIREBASE_SERVER_KEY = "AAAATN3fa7A:APA91bGMwD7zufPQIQHz1g_fKS4JOCk_bgwtQ_LG4ybypqIdHOS-t31o80l7Zbpn02GS6fyeGrx0qYeYFTypoydva0DQrVVyec1ESOVcGgo3s9a4D-i23fe62HBdDuT-f7xfIXvgaG6-";
 	private static final String FIREBASE_API_URL = "https://fcm.googleapis.com/fcm/send";
 
+	private static final Logger logger = LoggerFactory.getLogger(NotificationSender.class);
+
 	@Async
 	public CompletableFuture<String> send(HttpEntity<String> entity) {
 
@@ -37,9 +43,8 @@ public class NotificationSender {
 		interceptors.add(new HeaderRequestInterceptor("Authorization", "key=" + FIREBASE_SERVER_KEY));
 		interceptors.add(new HeaderRequestInterceptor("Content-Type", "application/json"));
 		restTemplate.setInterceptors(interceptors);
-
 		String firebaseResponse = restTemplate.postForObject(FIREBASE_API_URL, entity, String.class);
-
+		logger.info("firebase response is " + firebaseResponse);
 		return CompletableFuture.completedFuture(firebaseResponse);
 	}
 
@@ -50,6 +55,7 @@ public class NotificationSender {
 		if (type != null) {
 			JSONObject body = new JSONObject();
 			System.out.println("token is " + token);
+			logger.info("token is " + token);
 			JSONObject notification = new JSONObject();
 			notification.put(TITLE_IOS, title);
 			notification.put(BODY_IOS, titleBody);
@@ -60,7 +66,7 @@ public class NotificationSender {
 			for (Map.Entry<String, String> entry : dataMap.entrySet()) {
 				data.put(entry.getKey(), entry.getValue());
 			}
-
+			logger.info("sending to token " + token);
 			message.put("to", token);
 			message.put(NOTIFICATION, notification);
 			message.put(DATA, data);
@@ -81,7 +87,7 @@ public class NotificationSender {
 
 			body.put(NOTIFICATION, notification);
 			body.put(DATA, data);
-
+			logger.info("sending data " + data);
 			message.put("to", token);
 			message.put("data", data);
 		}
@@ -91,8 +97,10 @@ public class NotificationSender {
 		CompletableFuture<String> pushNotification = send(request);
 		try {
 			String string = pushNotification.get();
+			logger.info("notification sent successfully " + string);
 			System.out.println("success hai " + string);
 		} catch (InterruptedException | ExecutionException e) {
+			logger.error("error while sending notification " + e.getMessage());
 			throw new Exception("Failed to send notification");
 		}
 	}
